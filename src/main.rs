d7d90d85eaea6800fb632b34e15edba905183ba9
Jan De Landtsheer <jan@delandtsheer.eu>
2020-06-23 18:54:12 +0200

    cool

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,62 @@
+use anyhow::Result;
 #[allow(unused_imports)]
-
 use std::path::PathBuf;
 use structopt::StructOpt;
-use anyhow::Result;
 
+use std::marker::Unpin;
 use std::string::String;
 
 use async_std::prelude::*;
-use async_std::{task::block_on, io,io::BufReader};
+use async_std::{io, io::BufReader, io::Read, task::block_on};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "lsrt", about = "lexical sorter")]
 struct Opt {
     #[structopt(short, long)]
     debug: bool,
-    #[structopt(short = "k", long = "key")] 
+    #[structopt(short = "k", long = "key")]
     key: i8,
-    #[structopt(short = "H", long ="human")]
+    #[structopt(short = "H", long = "human")]
     human: bool,
     #[structopt(short = "F", long = "fieldsep")]
-    sep: String,
+    sep: char,
+    #[structopt(short = "l", long = "lines")]
+    lines: u32,
 
     file: Vec<PathBuf>,
-
 }
 
-async fn fillgrid(sep: &String) -> Result<Vec<Vec<String>>> {
+async fn fillgrid<R: Read + Unpin>(
+    fd: &mut io::BufReader<R>,
+    sep: &char,
+) -> Result<Vec<Vec<String>>> {
     let mut grid: Vec<Vec<String>> = vec![];
-    let stdin = io::stdin();
-    let reader = BufReader::new(stdin);
-    let mut lines = reader.lines();
+    let mut lines = fd.lines();
     while let Some(line) = lines.next().await {
-        let row:Vec<String> = line
-          .unwrap()
-          .split(sep)
-          .skip_while(|s| s == &"")
-          .map(|s| s.trim().to_string())
-          .collect();
-        println!("{:?}",row);
+        let row: Vec<String> = line
+            .unwrap()
+            .split(|c| c == *sep || c == '\t')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().to_string())
+            .collect();
         grid.push(row);
     }
     Ok(grid)
-
 }
 fn main() -> Result<()> {
     let opt = Opt::from_args();
-    if opt.file.len() == 0 {
-        let fut = fillgrid(&opt.sep);
-        let buf = block_on(fut);
-        println!("{:?}",buf.unwrap());
-    }
+    let buf = match opt.file.len() {
+        0 => {
+            let fd = io::stdin();
+            let mut reader = BufReader::new(fd);
+            let parsed_fut = fillgrid(&mut reader, &opt.sep);
+            let buf = block_on(parsed_fut);
+            buf
+        }
+
+        _ => Err(anyhow::anyhow!("no parsable strings found")),
+    };
+    println!("{:?}", buf);
 
     Ok(())
 }
